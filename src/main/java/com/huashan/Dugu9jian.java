c2104c3668284a292c74b6e154f2139855ba8c3f
linghuchong <1204544066@qq.com>
2021-03-09 19:02:06 +0800

    独孤九剑心法

---
+++ src/main/java/com/huashan/Dugu9jian.java
@@ -0,0 +1,10 @@
+package com.huashan;
+
+public class Dugu9jian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑！");
+        System.out.println("独孤九剑！");
+        System.out.println("独孤九剑！");
+        System.out.println("独孤九剑！");
+    }
+}
