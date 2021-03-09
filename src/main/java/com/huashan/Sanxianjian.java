89edb190aad5b77a4081a99f0ffff9949e8bb18b
linghuchong <1204544066@qq.com>
2021-03-09 22:50:39 +0800

    新剑法

---
+++ src/main/java/com/huashan/Sanxianjian.java
@@ -0,0 +1,13 @@
+package com.huashan;
+
+public class Sanxianjian {
+    public static void main(String[] args) {
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+    }
+}
